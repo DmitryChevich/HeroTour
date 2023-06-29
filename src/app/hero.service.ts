6aa2927c5c866b91b582ca912cd6261b492516b0
Dima Rogachevich <dima.rogachevich@gmail.com>
2023-06-29 18:42:21 +0300

    добаил месседж и месседж сервиз

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +11,6 @@ export class HeroService {
     const heroes = of(HEROES);
     return heroes;
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   
 }
