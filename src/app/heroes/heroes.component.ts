c96cf1d8b330c5aac254ea91f9f29088faed6c1b
Dima Rogachevich <dima.rogachevich@gmail.com>
2023-06-29 11:41:37 +0300

    кнопки кликабельны

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,10 @@ export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
-    
   };
   heroes = HEROES
+  selectedHero?: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 }
