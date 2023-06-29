e201833da3ec1ee064e215a44701e4a5710fe0aa
Dima Rogachevich <dima.rogachevich@gmail.com>
2023-06-29 10:35:32 +0300

    начал делать список героев

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,5 +10,7 @@ export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
+    
   };
+  heroes = HEROES
 }
